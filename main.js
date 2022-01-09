e46e9b93d69a12b0c63eb5dab173f2ea174ec540
vegtazain <atim4077@gmail.com>
2022-01-09 23:34:32 +0530

    second commit

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "slide",
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    autoplay:{
-        delay: 2500,
-        disableOnInteraction: false,
+// var swiper = new Swiper(".mySwiper", {
+//     effect: "slide",
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     autoplay:{
+//         delay: 2500,
+//         disableOnInteraction: false,
 
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+  var showMore=document.querySelector('.show-more')
+  var hideMore=document.querySelector('.hide-more')
+  var aboutEminem=document.querySelector('.about-eminem')
+
+  showMore.addEventListener('click', ()=>{
+    aboutEminem.classList.toggle('about-active')
+    showMore.classList.toggle('d-none')
+    hideMore.classList.toggle('d-none')
+  })
+  hideMore.addEventListener('click', ()=>{
+    aboutEminem.classList.toggle('about-active')
+    showMore.classList.toggle('d-none')
+    hideMore.classList.toggle('d-none')
+  })
