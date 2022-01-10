bd1d1c4e9b40454cb9abc6a03687e3947afd52ca
anand pandit <93030684+devjs1000@users.noreply.github.com>
2022-01-10 13:37:44 +0530

    service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+
+  
+var cacheName='vtApk69'
+
+//calling install event
+self.addEventListener('install',e=>{
+})
+
+//calling activate event
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(cacheNames=>{
+      return Promise.all(
+      cacheNames.map(cache=>{
+        if(cache!==cacheName){
+          return caches.delete(cache)
+        }
+      })
+      )
+    })
+    )
+})
+
+//calling fetch event
+self.addEventListener('fetch',e=>{
+  
+  e.respondWith(fetch(e.request).then(res=>{
+    const resClone=res.clone()
+    
+    caches.open(cacheName).then(cache=>{
+      cache.put(e.request, resClone);
+    })
+    return res;
+  }).catch(err=>caches.match(e.request).then(res=>res))
+  )
+})
